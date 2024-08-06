4a465a74dad722debd025576a0c90e6a844db166
thekshitij <159469775+thekshitij@users.noreply.github.com>
2024-08-06 23:21:40 +0530

    Add files via upload

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Footer.css";
+import { Fade } from "react-reveal";
+
+/* eslint-disable jsx-a11y/accessible-emoji */
+
+export default function Footer(props) {
+  return (
+    <div className="footer-div">
+      <Fade>
+       
+        {/* <ToggleSwitch theme={props.theme} onToggle={props.onToggle}/> */}
+      </Fade>
+    </div>
+  );
+}
