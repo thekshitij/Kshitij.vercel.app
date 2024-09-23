b26fd68bba73d5a40afa9d3f193a0660cfbb5257
thekshitij <159469775+thekshitij@users.noreply.github.com>
2024-09-23 20:14:45 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -25,7 +25,7 @@ const greeting = {
   subTitle:
     "I am an engineering student due to graduate in 2025,proficient in web development, neural networks and Python,I am Eager to continue pushing boundaries and making meaningful contributions in the tech industry.",
   resumeLink: 
-    "https://drive.google.com/file/d/10DcFhPNmpjNoyXMF10Epi3edCChdrBOA/view",
+    "https://drive.google.com/file/d/1h8IbrtCiK8UuugPtNhxeZdEFIWjDUTS_/view?usp=sharing",
   portfolio_repository: "https://github.com/thekshitij",
   githubProfile: "https://github.com/thekshitij",
 };
